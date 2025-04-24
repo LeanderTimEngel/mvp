7a6933c7ed40cacf89e0d28cf4050f148bbea928
LeanderTimEngel <leanderengel99@gmail.com>
2025-04-24 10:27:11 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
       </head>
-      <body className={`${inter.className} antialiased`}>
+      <body suppressHydrationWarning className={`${inter.className} antialiased`}>
         {children}
         <Toaster />
       </body>
